7dabdde73a6f1352ad45835d43f13b86215bf3cf
Chameena <hviparya94@gmail.com>
2018-11-06 13:52:36 +0530

    UI

---
+++ ACHSGate/commonInfo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ACHSGate
+{
+    /// <summary>
+    /// Interaction logic for commonInfo.xaml
+    /// </summary>
+    public partial class commonInfo : Window
+    {
+        public commonInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

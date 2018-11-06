a7171b164ef2e05421b28f341a17142ca8fb0797
Chameena <hviparya94@gmail.com>
2018-11-06 16:57:41 +0530

    ui

--- ACHSGate/homeAdmin.xaml.cs
+++ ACHSGate/homeAdmin.xaml.cs
@@ -61,6 +61,7 @@ namespace ACHSGate
         private void sign_off_Click(object sender, RoutedEventArgs e)
         {
             MainWindow main = new MainWindow();
+            this.Close();
             main.ShowDialog();
         }
     }
